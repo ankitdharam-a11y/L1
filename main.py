173b56856382e9086097b756376757f997b59645
ankitdharam-a11y <ankitdharam@gmail.com>
2025-11-29 06:47:57 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#Greeting you
+print("Hello,world!")
+
+#Intriduceing
+print("My name is Alex")
+
+#Telling his favirout game
+print("I love playing soccer")
+
+#Telling his calculation
+print("I result 3+5 is:8")
